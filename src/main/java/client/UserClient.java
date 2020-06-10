cbd0f3e36e753d426de84ebae7b6b1cd571fe776
TarasMalynovskyy <tarasmalynovskyy@gmail.com>
2020-06-10 15:52:11 +0300

    Add new methods to UserClient, create userToJson method

--- src/main/java/client/UserClient.java
+++ src/main/java/client/UserClient.java
@@ -12,24 +12,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.requestSpecification;
 
 public class UserClient extends BaseClient {
     private final String createWithListUrl = "/user/createWithList";
+    private final String getUserByUsernameUrl = "/user/{username}";
 
     public Response createUsersWithList(final List<User> users) {
         final JSONArray requestParams = new JSONArray();
         for (final User user : users) {
-            JSONObject object = new JSONObject();
-            object.put("id", user.getId());
-            object.put("username", user.getUserName());
-            object.put("firstname", user.getFirstName());
-            object.put("lastname", user.getLastName());
-            object.put("email", user.getEmail());
-            object.put("password", user.getPassword());
-            object.put("phone", user.getPhone());
-            object.put("userstatus", user.getUserStatus());
-
-            requestParams.put(object);
+            requestParams.put(userToJson(user));
         }
 
         return given(baseRequestSpecification)
@@ -37,12 +29,38 @@ public class UserClient extends BaseClient {
                 .post(createWithListUrl);
     }
 
+    public Response getUserByUserName(final String username) {
+        return given(baseRequestSpecification)
+                .get(getUserByUsernameUrl, username);
+    }
+
+    public Response updateUserByUsername(final String username, final User user) {
+        return given(baseRequestSpecification)
+                .body(userToJson(user).toString())
+                .put(getUserByUsernameUrl, username);
+    }
+
+    private JSONObject userToJson(final User user) {
+        JSONObject object = new JSONObject();
+        object.put("id", user.getId());
+        object.put("username", user.getUserName());
+        object.put("firstname", user.getFirstName());
+        object.put("lastname", user.getLastName());
+        object.put("email", user.getEmail());
+        object.put("password", user.getPassword());
+        object.put("phone", user.getPhone());
+        object.put("userstatus", user.getUserStatus());
+
+        return object;
+    }
+
     public List<User> createRandomUsers() {
         final Faker faker = new Faker();
         final List<User> users = new ArrayList<>();
+
         for (int i = 0; i < 3; i++) {
             final User user = new User();
-            user.setId(i);
+            user.setId(i++);
             user.setUserName(faker.gameOfThrones().character());
             user.setFirstName(faker.name().firstName());
             user.setLastName(faker.name().lastName());
