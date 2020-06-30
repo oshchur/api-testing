ad7b2e105086def230489ca99f1a0022e9f593f2
TarasMalynovskyy <tarasmalynovskyy@gmail.com>
2020-06-30 12:04:06 +0300

    Add new tests, refactor assertions

--- src/main/java/assertion/BaseAssertion.java
+++ src/main/java/assertion/BaseAssertion.java
@@ -1,5 +1,6 @@
 package assertion;
 
+import io.restassured.internal.http.Status;
 import io.restassured.response.Response;
 import org.testng.asserts.SoftAssert;
 
@@ -12,6 +13,13 @@ public class BaseAssertion {
     public static void checkResponse(final Response response, final int statusCode) {
         final SoftAssert softAssert = new SoftAssert();
         softAssert.assertEquals(response.statusCode(), statusCode, "Error - incorrect status code");
+        softAssert.assertEquals(response.contentType(), "application/json");
+        softAssert.assertAll();
+    }
+
+    public static void checkResponse(final Response response, final Status statusCode) {
+        final SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(statusCode.matches(response.statusCode()), "Error - status code is out of bound expectation");
         softAssert.assertEquals(response.contentType(), "application/json");
         softAssert.assertAll();
     }
